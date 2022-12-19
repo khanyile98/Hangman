73989096be4a3518a03866d9c9a03eb9f8720b4d
Mr.khanyile(Developer) <67341641+khanyile98@users.noreply.github.com>
2022-12-19 12:50:32 +0200

    Add files via upload

---
+++ src/test/java/hangman/HangmanTests.java
@@ -0,0 +1,12 @@
+package hangman;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+
+public class HangmanTests {
+    @Test
+    public void mustBeTrue(){
+        assertTrue(true);
+    }
+}
